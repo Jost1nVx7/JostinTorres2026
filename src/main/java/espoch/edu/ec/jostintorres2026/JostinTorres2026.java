89b60ce4f3bcc929990e6b1ea0e4743f9cf89352
Jost1nVx7 <jostinjd2002@gmail.com>
2024-11-07 13:16:54 -0500

    Subido

---
+++ src/main/java/espoch/edu/ec/jostintorres2026/JostinTorres2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.jostintorres2026;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class JostinTorres2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
